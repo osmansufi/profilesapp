3fdd4f7f966ff2f96795fa00c0802bc2a779e445
Osman Sufi <sufiosman@gmail.com>
2025-09-05 22:52:22 -0400

    Added background image and changed website text slightly

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Osman Sufi</h1>
-      <p>Website under construction</p>
+      <footer>osman sufi</footer>
     </>
   )
 }
